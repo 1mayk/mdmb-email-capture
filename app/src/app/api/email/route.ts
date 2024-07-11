ed1a3c4e6864d917901ed50e798ed94f48cbdb15
Mayk <maycon.lrgo@gmail.com>
2024-07-11 12:17:05 -0300

    feat(): api finalizada

--- app/src/app/api/email/route.ts
+++ app/src/app/api/email/route.ts
@@ -1,14 +1,25 @@
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
+import { storeEmail, readEmails } from "./services";
 
 export async function GET() {
+  const emailArr = readEmails();
+
   return NextResponse.json({
-    email: [
-      {
-        email: "maycon",
-      },
-      {
-        id: 2,
-      },
-    ],
+    emailArr,
+  });
+}
+
+export async function POST(req: NextRequest) {
+  const data = await req.json();
+  const email = data.email;
+
+  storeEmail(email);
+  const arrEmail = readEmails()
+
+  return new NextResponse(arrEmail, {
+    status: 200,
+    headers: {
+      'Content-Type': 'text/plain',
+    },
   });
 }
---
+++ app/src/app/api/email/services.ts
@@ -0,0 +1,46 @@
+import * as fs from 'fs';
+import * as path from 'path';
+
+const filePath = path.join(__dirname, 'emails.json');
+
+// Função para armazenar um email em um documento JSON
+export function storeEmail(email: string): void | string {
+    let emails: string[] = [];
+
+    try {
+        // Verificar se o arquivo já existe e ler o conteúdo
+        if (fs.existsSync(filePath)) {
+            const data = fs.readFileSync(filePath, 'utf8');
+            emails = JSON.parse(data);
+        } else {
+            // Verificar se o diretório existe, caso contrário, criar
+            const dir = path.dirname(filePath);
+            if (!fs.existsSync(dir)) {
+                fs.mkdirSync(dir, { recursive: true });
+            }
+        }
+
+        // Adicionar o novo email ao array
+        emails.push(email);
+
+        // Escrever o array atualizado de volta ao arquivo JSON
+        fs.writeFileSync(filePath, JSON.stringify(emails, null, 2), 'utf8');
+    } catch (error) {
+        console.error('Erro ao armazenar o email:', error);
+    }
+}
+
+// Função para ler o conteúdo do arquivo e retornar em formato JSON
+export function readEmails(): string {
+    try {
+        if (!fs.existsSync(filePath)) {
+            return JSON.stringify([], null, 2); // Retornar um array vazio se o arquivo não existir
+        }
+
+        const data = fs.readFileSync(filePath, 'utf8');
+        return data;
+    } catch (error) {
+        console.error('Erro ao ler os emails:', error);
+        return JSON.stringify([], null, 2); // Retornar um array vazio em caso de erro
+    }
+}
